88705135b172e0813b6997974b152b42476f4d15
Richard Hughes <richard.j.hughes@hotmail.com>
2022-10-24 20:31:00 -0400

    Add tests for `run`.

--- fxml/src/conversion/xml_conversion.rs
+++ fxml/src/conversion/xml_conversion.rs
@@ -1,8 +1,26 @@
 pub fn convert(input: &str) -> String {
-    input.to_uppercase()
+    String::from(input)
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
+    #[test]
+    fn convert_empty_input_returns_empty_result() {
+        let input = "";
+
+        let result = convert(input);
+
+        assert_eq!("", result);
+    }
+
+    #[test]
+    fn convert_invalid_input_returns_input() {
+        let input = "invalid XML...";
+
+        let result = convert(input);
+
+        assert_eq!(input, result);
+    }
 }
--- fxml/src/lib.rs
+++ fxml/src/lib.rs
@@ -1,16 +1,12 @@
 pub mod settings;
 pub mod conversion;
 
-pub fn run(input: &String, config: &settings::config::Config) -> Result<(), Box<dyn std::error::Error>> {
+pub fn run(input: &String, config: &settings::config::Config) -> Result<String, Box<dyn std::error::Error>> {
     if config.help() {
-        println!("{}", help_text());
-        return Ok(());
+        return Ok(help_text());
     }
 
-    let result = conversion::xml_conversion::convert(&input);
-    print!("Result:\n{result}");
-
-    Ok(())
+    Ok(conversion::xml_conversion::convert(&input))
 }
 
 fn version() -> &'static str {
@@ -23,6 +19,8 @@ fn help_text() -> String {
 
 #[cfg(test)]
 mod tests {
+    use crate::conversion::xml_conversion::convert;
+
     use super::*;
 
     #[test]
@@ -35,5 +33,27 @@ mod tests {
     fn help_text_returns_value() {
         let result = help_text();
         assert!(!result.is_empty());
+    }
+
+    #[test]
+    fn run_display_help_text_returns_help_text() {
+        let input = String::from("text");
+        let config = settings::config::Config::new(&vec![String::from("--help")]);
+
+        let result = run(&input, &config);
+
+        assert!(result.is_ok());
+        assert_eq!(help_text(), result.ok().unwrap());
+    }
+
+    #[test]
+    fn run_converts_text_returns_converted_text() {
+        let input = String::from("<text/>");
+        let config = settings::config::Config::new(&vec![]);
+
+        let result = run(&input, &config);
+
+        assert!(result.is_ok());
+        assert_eq!(convert(&input), result.ok().unwrap());
     }
 }
